9fdebe5cdec55769ffb0696baa8f67ba43516c4b
lambe <wannes.lamberts@student.uantwerpen.be>
2024-10-18 19:12:56 +0200

    sort files + preprocess + calibrate

--- dataset.py
+++ dataset.py
@@ -4,6 +4,10 @@ from bs4 import BeautifulSoup
 import os
 import zipfile
 import pandas as pd
+import re
+import shutil
+import numpy as np
+from sklearn import linear_model
 
 #link towards the dataset
 LINK_DATASET = "https://ftp.pride.ebi.ac.uk/pride/data/archive/2017/02/PXD004732/"
@@ -156,6 +160,48 @@ def load_dataframe(path_csv,columns=None,seperator='\t'):
         df = df[columns]
     return df
 
+def preprocess_dataframe(df,format_modified_sequence = True,min_score = 90, max_PEP = 0.01,reset_index = True):
+    """
+    Preprocess the input DataFrame by formatting the 'Modified sequence' column,
+    and filtering based on the minimum score and maximum PEP values. At the end resets the index if chosen.
+
+    Parameters:
+    -----------
+    df : pandas.DataFrame
+        The DataFrame to be preprocessed.
+
+    format_modified_sequence : bool, optional
+        If True, removes the first and last character of the 'Modified sequence' column. Default is True.
+
+    min_score : float, optional
+        The minimum score threshold. Rows with a score less than this value will be removed. Default is 90.
+
+    max_PEP : float, optional
+        The maximum PEP threshold. Rows with PEP greater than this value will be removed. Default is 0.01.
+
+    reset_index : bool, optional
+        If True, resets the index of the dataframe, Default is True.
+
+    Returns:
+    --------
+    pandas.DataFrame
+        The preprocessed DataFrame
+    """
+
+    # format the 'Modified sequence' by removing the first and last character
+    if format_modified_sequence:
+        df["Modified sequence"] = df["Modified sequence"].str[1:-1]
+
+    # Filter rows based on the 'Score' and 'PEP' columns
+    df = df[df["Score"]>=min_score]
+    df = df[df["PEP"]<=max_PEP]
+
+    # Reset the index and drop the old index
+    if reset_index:
+        df = df.reset_index(drop=True)
+
+    return df
+
 def write_dataframe_to_file(df, output_path, separator='\t'):
     """
     Writes a pandas DataFrame to a CSV file with an optional custom separator.
@@ -178,8 +224,181 @@ def write_dataframe_to_file(df, output_path, separator='\t'):
     except Exception as e:
         print(f"An error occurred while writing the DataFrame to CSV: {e}")
 
-#scrape_dataset(LINK_DATASET,"zips",1)
+def sort_evidence_files(location):
+    """
+    Sorts evidence files into directories based on a pool number extracted from their filenames.
+
+    Parameters:
+    -----------
+    location : str
+        The path to the directory where the evidence files are stored.
+        All files in this directory will be checked, and sorted into subdirectories.
+    """
+    for filename in os.listdir(location):
+        # Search for "Pool_<number>" in the filename (case-insensitive)
+        pool = re.search("Pool_(\\d+)", filename,re.IGNORECASE).group(0).capitalize()
+
+        # Define the path of the pool directory and file location
+        directory = os.path.join(location, pool)
+        file_location = os.path.join(location,filename)
+        os.makedirs(directory, exist_ok=True)
+
+        #move file to the directory
+        shutil.move(file_location,directory)
+    print("Evidence files sorted")
+
+
+def get_calibration_peptides(df, calibration_df=None):
+    """
+    Retrieves a dictionary of calibration peptides and their corresponding iRT (indexed Retention Time) values.
+
+    Author:
+    -----------
+    Ceder Dens
+
+    Parameters:
+    -----------
+    df : pandas.DataFrame
+        The main DataFrame containing peptide data.
+
+    calibration_df : pandas.DataFrame, optional
+        A DataFrame containing reference peptides and their known iRT values. If provided, the function
+        will return calibration peptides that overlap between `df` and `calibration_df`. If not provided,
+        a default set of iRT calibration peptides will be used.
+
+    Returns:
+    --------
+    dict
+        A dictionary where the keys are peptide sequences (str) and the values are the corresponding iRT values (float).
+        If `calibration_df` is provided, the dictionary will contain peptides from the overlap of `df` and `calibration_df`.
+        Otherwise, a predefined set of calibration peptides and iRT values is returned.
+    """
+    if calibration_df is None:
+        return {
+            "TFAHTESHISK": -15.01839514765834,
+            "ISLGEHEGGGK": 0.0,
+            "LSSGYDGTSYK": 12.06522819926421,
+            "LYSYYSSTESK": 31.058963905737304,
+            "GFLDYESTGAK": 63.66113155016407,
+            "HDTVFGSYLYK": 72.10102416227504,
+            "ASDLLSGYYIK": 90.51605846673961,
+            "GFVIDDGLITK": 100.0,
+            "GASDFLSFAVK": 112.37148254946804,
+        }
+    else:
+        overlap = df.merge(calibration_df, how="inner", on="Modified sequence")
+        return {
+            k: v for k, v in zip(overlap["Modified sequence"], overlap["iRT"])
+        }
+
+def calibrate_to_iRT(df,calibration_df=None,seq_col="Modified sequence",rt_col="Retention time",
+    irt_col="iRT",plot=False,filename=None,take_median=False,):
+    """
+    Calibrates the retention times in a DataFrame to indexed Retention Time (iRT) values using a set of calibration peptides.
+
+    Parameters:
+    -----------
+    df : pandas.DataFrame
+        The input DataFrame containing peptide sequences and their respective retention times
+
+    calibration_df : pandas.DataFrame, optional
+        A DataFrame containing calibration peptides and their known iRT values. If not provided, a predefined
+        set of calibration peptides will be used.
+
+    seq_col : str, optional
+        The column name in `df` that contains the peptide sequences. Default is "Modified sequence".
+
+    rt_col : str, optional
+        The column name in `df` that contains the retention time values. Default is "Retention time".
+
+    irt_col : str, optional
+        The column name where the predicted iRT values will be stored in `df`. Default is "iRT".
+
+    plot : bool, optional
+        If True, a scatter plot of the original Retention time values vs. iRT values will be generated along with the fitted regression line.
+
+    filename : str, optional
+        If provided, the function will print the number of calibration peptides found in the DataFrame. Useful for logging or debugging.
+
+    take_median : bool, optional
+        If True, the median retention time for each calibration peptide will be used. Otherwise, the first occurrence of the Retention time value will be used.
+
+    Returns:
+    --------
+    pandas.DataFrame or None
+        The input DataFrame with an additional column containing the calibrated iRT values.
+        If fewer than two calibration peptides are found in the input data, the function returns `None`.
+
+    Process:
+    --------
+    1. The function first retrieves a dictionary of calibration peptides and their corresponding iRT values.
+    2. It loops through the calibration peptides and retrieves the corresponding Retention time values from the input DataFrame.
+    3. If `take_median` is True, it uses the median Retention time value for each peptide; otherwise, it uses the first occurrence.
+    4. The old Retention time values and iRT values are then used to fit a linear regression model.
+    5. The model is used to predict iRT values for all peptides in the input DataFrame.
+    6. If `plot` is True, a scatter plot of calibration points and the regression line is displayed.
+    7. The function returns the input DataFrame with an additional column for iRT values, or `None` if calibration fails.
+    """
+
+    # Get calibration peptides and their corresponding iRT values
+    calibration_peptides = get_calibration_peptides(df, calibration_df)
+    old_rt = []
+    cal_rt = []
+
+    # Loop through each calibration peptide
+    for pep, iRT in calibration_peptides.items():
+        # Filter the DataFrame to get rows corresponding to the current peptide sequence
+        pep_df = df[df[seq_col] == pep]
+        if len(pep_df) > 0:
+            # Use the median or first occurrence of the RT value based on the `take_median` flag
+            if take_median:
+                old = np.median(df[df[seq_col] == pep][rt_col])
+            else:
+                old = df[df[seq_col] == pep][rt_col].iloc[0]
+
+            old_rt.append(old)
+            cal_rt.append(iRT)
+    # Log the number of calibration peptides found if `filename` is provided
+    if filename is not None:
+        print(
+            f"{filename} had {len(old_rt)}/{len(calibration_peptides)} calibration peptides"
+        )
+    # If fewer than two calibration peptides are found, return None (unable to perform calibration)
+    if len(old_rt) < 2:
+        return None
+
+    # Fit a linear regression model using the original RT values and the iRT values
+    regr = linear_model.LinearRegression()
+    regr.fit(np.array(old_rt).reshape(-1, 1), np.array(cal_rt).reshape(-1, 1))
+
+    # Predict iRT values for all peptides in the input DataFrame
+    df[irt_col] = regr.predict(df[rt_col].values.reshape(-1, 1))
+
+    # Plot the calibration points and the fitted regression line if `plot=True`
+    if plot:
+        plt.scatter(old_rt, cal_rt, label="calibration points")
+        plt.plot(
+            range(int(min(old_rt) - 5), int(max(old_rt) + 5)),
+            regr.predict(
+                np.array(
+                    range(int(min(old_rt) - 5), int(max(old_rt) + 5))
+                ).reshape(-1, 1)
+            ),
+            label="fitted regressor",
+        )
+        plt.legend()
+        plt.show()
+
+    return df
+
+
+#scrape_dataset(LINK_DATASET,"zips")
 #extract_file_from_zip("data","zips")
 #merge_tabular_files("data", "evidence_combined.tsv")
 #df = load_dataframe("evidence_combined.tsv",["Modified sequence","Retention time","Score","Experiment"])
 #write_dataframe_to_file(df,"simple_dataframe.tsv")
+#sort_evidence_files("data")
+df = load_dataframe("data/Pool_2/Thermo_SRM_Pool_2_01_01_2xIT_2xHCD-1h-R2-tryptic-evidence.txt",["Modified sequence","Retention time","Score","PEP","Experiment"])
+df = preprocess_dataframe(df)
+df= calibrate_to_iRT(df)
+
