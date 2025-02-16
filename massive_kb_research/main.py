c5676283666874b9ede6b7938ef4dda1904ae0b0
WannesLamberts <Wannes.Lamberts@student.uantwerpen.be>
2025-02-16 16:02:13 +0100

    Preparing supercomputer run

--- massive_kb_research/main.py
+++ massive_kb_research/main.py
@@ -1,7 +1,10 @@
+import sys
+
 from utils.preprocess import *
 import multiprocessing
 
-RESULTS_PATH = "../../massivekb_dataset/working_results"
 if __name__ == '__main__':
+    psms_path = sys.argv[1]
+    output_path = sys.argv[2]
     chronologer = load_dataframe("datasets/chronologer.tsv")
-    calibrate_directory(RESULTS_PATH+'/results/psms', RESULTS_PATH+'/results_calibrated2', chronologer)
+    calibrate_directory(psms_path, output_path, chronologer)
