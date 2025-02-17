3f4ef4c0f5c09a4afcf7b461510b888040293249
WannesLamberts <Wannes.Lamberts@student.uantwerpen.be>
2025-02-17 11:24:46 +0100

    100 run

--- massive_kb_research/testing.py
+++ massive_kb_research/testing.py
@@ -1,7 +1,5 @@
 from utils.preprocess import *
-merge_psm_files('test_data/psms_dups','dups.tsv',['filename','scan','RT','sequence','mztab_filename','task_id'])
-df = pd.read_csv('dups.tsv', sep='\t',index_col=False)
-print(df.info())
-merge_psm_files('test_data/psms_drop_dups','drop_dups.tsv',['filename','scan','RT','sequence','mztab_filename','task_id'])
-df = pd.read_csv('drop_dups.tsv', sep='\t',index_col=False)
-print(df.info())
+df = pd.read_csv('te.tsv', sep='\t', index_col=False)
+print(len(df))
+print(df.columns)
+print(df['sequence'].nunique())
